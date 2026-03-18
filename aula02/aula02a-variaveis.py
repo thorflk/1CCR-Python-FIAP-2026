c3da1d4eeb7e22fb5c652c4fbf39b19c8aa511f2
thorflk <thorfcamargo21@gmail.com>
2026-03-18 20:47:07 -0300

    aula 2 - variáveis

---
+++ aula02/aula02a-variaveis.py
@@ -0,0 +1,37 @@
+print('ola mundo')
+
+print(7+4)
+print("7+4")
+print('7'+'4') #CONCATENANDO STRINGS
+
+#Comentário de 1 linha
+'''
+    Comentários de
+    múltiplas
+    linhas
+'''
+
+#VARIÁVEIS
+nome = "Thor" #str
+idade = 18 #int
+peso = 60.0 #float
+
+print (nome, idade, peso)
+print(f"Oiiii",nome, "!!!!!!!")
+
+#INPUT - SIMULAÇÃO DE UM FORMS NO CMD
+nome = input('Digite seu nome: ')
+idade = int(input("Digite sua idade: "))
+peso = float(input('Digite seu peso: '))
+
+print(nome, idade, peso)
+print(idade + 1)
+
+ano_nasc = 1999
+ano_atual = 2026
+idade = ano_atual - ano_nasc
+print(idade)
+
+
+
+
---
+++ aula02/desafios.py
@@ -0,0 +1,13 @@
+nome = input("Digite seu nome: ")
+print('Seja bem-vindo à FIAP', nome)
+
+dia = input('Digite seu dia de  nascimento: ')
+mes = input('Digite seu mês de nascimento: ')
+ano = input('Digite seu ano de nascimento: ')
+
+print(f"{dia}/{mes}/{ano}")
+
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número para soma: '))
+soma = num1 + num2
+print('A soma é igual a', soma)
